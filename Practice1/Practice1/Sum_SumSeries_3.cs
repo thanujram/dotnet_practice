7f631b48ee30bb8bec99688f6a5708a2a2972caa
thanujram01 <thanujram13@gmail.com>
2026-09-11 09:33:55 +0530

    Sum of Sum series 3

---
+++ Practice1/Practice1/Sum_SumSeries_3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    internal class Sum_SumSeries_3
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum of Sum Series : " + Sum_SumSer3(number));
+        }
+
+        static int Sum_SumSer3(int n)
+        {
+            return ((n * (n + 1) * (n + 2)) / 6);
+        }
+    }
+}
